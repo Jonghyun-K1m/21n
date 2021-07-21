8415e21e93f4ca928a5cd190cb94aabc3dbd032e
Jonghyun-K1m <37470085+Jonghyun-K1m@users.noreply.github.com>
2021-07-21 13:16:06 +0900

    Create first_dto.java

---
+++ src/dto/first_dto.java
@@ -0,0 +1,30 @@
+package dto;
+
+public class first_dto {
+	
+	private String u_id;
+	private String u_pw;
+	private String u_name;
+	
+
+	
+	public String getU_name() {
+		return u_name;
+	}
+	public void setU_name(String u_name) {
+		this.u_name = u_name;
+	}
+	public String getU_pw() {
+		return u_pw;
+	}
+	public void setU_pw(String u_pw) {
+		this.u_pw = u_pw;
+	}
+	public String getU_id() {
+		return u_id;
+	}
+	public void setU_id(String u_id) {
+		this.u_id = u_id;
+	}
+	
+}
