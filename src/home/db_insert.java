15ddad5f074c1afbf7d5141da8b62f9359d450a4
Jonghyun-K1m <37470085+Jonghyun-K1m@users.noreply.github.com>
2022-05-02 09:18:39 +0900

    Update db_insert.java
    개발버전과 동기화

--- src/home/db_insert.java
+++ src/home/db_insert.java
@@ -1,6 +1,9 @@
 package home;
-
+import com.jh.Crpto.*;
+import com.jh.config.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,6 +26,7 @@ public class db_insert extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    /*
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("service called");
 		String a = request.getParameter("stock_a");
@@ -33,6 +37,34 @@ public class db_insert extends HttpServlet {
 		first_dao StockDao=new first_dao();
 		int ResultValue=StockDao.db_insert(a,b);
 		System.out.println(ResultValue);
+        super.service(request, response);
+        
+    }
+    
+    */
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("db insert service called");
+	
+		String sk_str = (String) request.getSession().getAttribute("sk_str");		
+		String a = (String) request.getSession().getAttribute("stock_a");
+		String b = (String) request.getSession().getAttribute("stock_b");	
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(sk_str);
+		
+		first_dao StockDao=new first_dao();
+		int ResultValue=StockDao.db_insert(a,b);
+		System.out.println(ResultValue);
+		
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html><head></head><body>");
+		out.println("<br><a href='LoginSuccess.jsp'>return</a>");
+		out.println("</body></html>");
+		out.close();
+
         super.service(request, response);
         
     }
